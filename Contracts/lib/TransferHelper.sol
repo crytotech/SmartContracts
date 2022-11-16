802a31c82817d2eb31faa5841221e212c2f2dcb1
git <eae966@gmail.com>
2022-11-16 22:40:14 +0330

    Big updates.

--- Contracts/lib/TransferHelper.sol
+++ Contracts/lib/TransferHelper.sol
@@ -1,4 +1,6 @@
 pragma solidity >=0.6.0;
+
+// helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
 library TransferHelper {
     function safeApprove(address token, address to, uint value) internal {
         // bytes4(keccak256(bytes('approve(address,uint256)')));
